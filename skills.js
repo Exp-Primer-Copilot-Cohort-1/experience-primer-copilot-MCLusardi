4ed4fc5b81fdbe9af0d3a1dd6e0eb55c4a79df69
MCLusardi <56372378+MCLusardi@users.noreply.github.com>
2023-12-26 20:30:38 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,17 @@
+function calculateNumbers(var1, var2)  {
+    console.log(var1 + var2);
+}
+
+function calculateNumbers(var1, var2)  {
+    console.log(var1 - var2);
+}
+function calculateNumbers(var1, var2)  {
+    console.log(var1 * var2);
+}
+function calculateNumbers(var1, var2)  {
+    console.log(var1 / var2);
+}
+
+function calculateNumbers(var1, var2)  {
+    console.log(var1 % var2);
+}
